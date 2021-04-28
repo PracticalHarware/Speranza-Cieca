9fc3090fa7489bf956e8863eeb2fa028e6fcc64b
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2021-04-28 14:51:49 +0530

    Modulised the program

--- OpenCVModules/PlainFaceRecog.py
+++ OpenCVModules/PlainFaceRecog.py
@@ -6,30 +6,31 @@ faceCascade = cv2.CascadeClassifier('E:\OpenCV\opencv\sources\data\haarcascades\
 
 video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
+def plainFaceRecog():
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30)
-        
-    )
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30)
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        )
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('Video', frame)
 
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything is done, release the capture
+    video_capture.release()
+    cv2.destroyAllWindows()
