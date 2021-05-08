9702fa9b310a2abc07bffc54b3d2128341e71284
Nihal Srivastava <82692408+Nihal-Srivastava05@users.noreply.github.com>
2021-05-08 14:24:30 +0530

    Create ReadTextFromCamera.py
    Added a module that works like OCR i.e converts the text visible from camera to string file and prints it out.

---
+++ OpenCVModules/ReadTextFromCamera.py
@@ -0,0 +1,40 @@
+
+import easyocr
+import cv2
+import random
+import time
+import os
+
+def scan_read():
+
+    cap = cv2.VideoCapture(0);
+    reader = easyocr.Reader(['en'], gpu= False);
+    
+    success, img = cap.read();
+    
+    if not success:
+        print('Failed to take picture')
+    else:
+        cv2.imshow("Image", img);
+        
+        time.sleep(5)
+        
+        r = random.randint(1, 10000000)
+        img_name = 'image-'+str(r)+'.png'
+        cv2.imwrite(img_name, img);
+        
+        time.sleep(5)
+        
+        result = reader.readtext(img_name);
+        
+        for detection in result:
+            text = detection[1]
+            print(text);
+            
+        os.remove(img_name);
+            
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    scan_read();
